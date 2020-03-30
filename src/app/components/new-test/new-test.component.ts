56fba14757fcfb829fe3a5b0bd80870e0492b961
Alex Masliychuk <alexmasliychuk@gmail.com>
2020-03-30 18:36:59 +0300

    csv file data validation added

--- src/app/components/new-test/new-test.component.ts
+++ src/app/components/new-test/new-test.component.ts
@@ -11,6 +11,7 @@ export class NewTestComponent implements OnInit {
   formGroup;
   parsedCsvFile = null;
   fileName: null | string = null;
+  error: null | string = null;
 
   constructor(private papa: Papa) { }
 
@@ -18,7 +19,34 @@ export class NewTestComponent implements OnInit {
   }
 
   onStartClicked() {
+  }
 
+  private validURL(str) {
+    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+      '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    return !!pattern.test(str);
+  }
+
+  /**
+   * Check if csv file data are correct.
+   * Only urls should exist
+   */
+  private isValidData(data: Array<Array<String>>): boolean{
+    for (let i = 0; i < data.length; i++) {
+      let el = data[i];
+      try {
+        if (this.validURL(el[0]) === false) {
+          return false;
+        }
+      } catch (error) {
+        return false;
+      }
+    }
+    return true;
   }
 
   onFileChange(event) {
@@ -32,7 +60,12 @@ export class NewTestComponent implements OnInit {
       this.papa.parse(file, {
         complete: (results) => {
           console.log('Parsed CSV: ', results);
-          this.parsedCsvFile = results;
+          if (this.isValidData(results.data) && results.data.length > 0) {
+            this.error = null;
+            this.parsedCsvFile = results;
+          } else {
+            this.error = 'Selected CSV file format is wrong or empty.'
+          }
         }
       });
 
