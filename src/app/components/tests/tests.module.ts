8b59f87ee99debb1f9174c2477f6b20143c7dcbc
Alex Masliychuk <alexmasliychuk@gmail.com>
2020-03-31 17:37:27 +0300

    tooltip module added

--- src/app/components/tests/tests.module.ts
+++ src/app/components/tests/tests.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { TestsRoutingModule } from './tests-routing.module';
 import { TestsComponent } from './tests.component';
+import { TooltipModule } from 'ngx-bootstrap';
 
 
 @NgModule({
   declarations: [TestsComponent],
   imports: [
     CommonModule,
-    TestsRoutingModule
+    TestsRoutingModule,
+    TooltipModule.forRoot()
   ]
 })
 export class TestsModule { }
