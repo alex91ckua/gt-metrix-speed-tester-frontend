da48edfd125db395f4697cf4c80888476b32241c
Alex Masliychuk <alexmasliychuk@gmail.com>
2020-03-31 18:10:20 +0300

    check tests automatically

--- src/app/components/tests/tests.component.ts
+++ src/app/components/tests/tests.component.ts
@@ -11,14 +11,39 @@ export class TestsComponent implements OnInit {
 
   loaded = false;
   tests: Test[];
+  intervalIds = [];
+  readonly UPDATE_INTERVAL = 10000; // in seconds
 
   constructor(private gtMetrixService: GtMetrixService) { }
+
+  initIntervalCheck() {
+    this.tests.forEach((test, i) => {
+      if ( ['queued', 'started'].includes(test.state) ) {
+        const intervalId = setInterval(() => {
+          test.state = 'updating now';
+          this.gtMetrixService.getTest(test.id).subscribe((data) => {
+            // update test data
+            this.tests[i] = data;
+          });
+        }, this.UPDATE_INTERVAL);
+
+        this.intervalIds.push(intervalId);
+      }
+    });
+  }
 
   ngOnInit() {
     this.gtMetrixService.getTests().subscribe((data) => {
       this.tests = data;
       this.loaded = true;
+      this.initIntervalCheck();
     })
+  }
+
+  ngOnDestroy() {
+    this.intervalIds.forEach(id => {
+      clearInterval(id);
+    });
   }
 
 }
