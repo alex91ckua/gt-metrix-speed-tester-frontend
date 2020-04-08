8be3c820d214dd1d7360d56226e2167cdb6e64be
Alex Masliychuk <alexmasliychuk@gmail.com>
2020-04-08 11:06:05 +0300

    font awesome added to tests list

--- src/app/components/tests/tests.component.ts
+++ src/app/components/tests/tests.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GtMetrixService } from 'src/app/core/services/gt-metrix.service';
 import { Test } from 'src/app/core/models/test.type';
+import { faSync } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-tests',
@@ -9,6 +10,7 @@ import { Test } from 'src/app/core/models/test.type';
 })
 export class TestsComponent implements OnInit {
 
+  faSync = faSync;
   loaded = false;
   tests: Test[];
   intervalIds = [];
@@ -20,7 +22,6 @@ export class TestsComponent implements OnInit {
     this.tests.forEach((test, i) => {
       if ( ['queued', 'started'].includes(test.state) ) {
         const intervalId = setInterval(() => {
-          test.state = 'updating now';
           this.gtMetrixService.getTest(test.id).subscribe((data) => {
             // update test data
             this.tests[i] = data;
--- src/app/components/tests/tests.module.ts
+++ src/app/components/tests/tests.module.ts
@@ -4,14 +4,15 @@ import { CommonModule } from '@angular/common';
 import { TestsRoutingModule } from './tests-routing.module';
 import { TestsComponent } from './tests.component';
 import { TooltipModule } from 'ngx-bootstrap';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [TestsComponent],
   imports: [
     CommonModule,
     TestsRoutingModule,
-    TooltipModule.forRoot()
+    TooltipModule.forRoot(),
+    FontAwesomeModule
   ]
 })
 export class TestsModule { }
